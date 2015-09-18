9c655e7fbcd2c407979d7173dba5639f143bc035
Adam Dunlap <dunlapadam@gmail.com>
2015-09-17 20:39:58 -0700

    Simplifying and improving test

--- papi-simple-tests.cpp
+++ papi-simple-tests.cpp
@@ -1,68 +1,47 @@
-// -*- C++ -*-
-// Main0.cc
-// cs181j hw1 example
-// A simple file to illustrate how to use PAPI to measure things
-// Jeff Amelang, 2015
-
-// c junk
-#include <cstdio>
-#include <cstdlib>
-#include <cmath>
-
-// c++ junk
-#include <array>
-#include <vector>
-#include <algorithm>
-#include <chrono>
-#include <random>
-
-// All the magic for measuring cache misses is in papi
 #include "papi-simple.h"
 
+// c junk
+#include <cmath>
+#include <vector>
+#include <random>
+#include <iostream>
+
+#include <cassert>
+
 // Reduce the amount of typing we have to do for timing things.
-using std::chrono::high_resolution_clock;
-using std::chrono::duration;
-using std::chrono::duration_cast;
 using std::size_t;
+using std::cout;
+using std::endl;
 
 int main() {
-  PAPI papi({PAPI_FDV_INS});
 
-  // Create a c++11 random number generator
-  std::mt19937 randomNumberEngine;
-  std::uniform_real_distribution<double> randomNumberGenerator(0, 1);
+    for (size_t i=0; i<10; ++i) {
+        PAPI papi({PAPI_FDV_INS});
+  
+        // Create a c++11 random number generator
+        std::mt19937 randomNumberEngine;
+        std::uniform_real_distribution<double> randomNumberGenerator(0, 1);
+  
+  
+        // We'll record how many floating point divides are used to generate a
+        //  bunch of random numbers, because why not?
+        const size_t numberOfRandomNumbers = 100000000;
+        double randomNumberSum = 0;
 
-  // Start timing
-  const high_resolution_clock::time_point tic =
-    high_resolution_clock::now();
-  papi.start_counting();
-
-
-  // We'll record how many floating point divides are used to generate a
-  //  bunch of random numbers, because why not?
-  const size_t numberOfRandomNumbers = 1e8;
-  double randomNumberSum = 0;
-  for (size_t i = 0; i < numberOfRandomNumbers; ++i) {
-    const double randomNumberBetween0And1 =
-      randomNumberGenerator(randomNumberEngine);
-    randomNumberSum += randomNumberBetween0And1;
-  }
-
-  // Stop timing
-  const high_resolution_clock::time_point toc =
-    high_resolution_clock::now();
-  const size_t numberOfDivisions = papi.stop_counting()[0];
-  const double elapsedTime =
-    duration_cast<duration<double> >(toc - tic).count();
-
-  printf("randomNumberSum was %11.4e, which is %%%5.1f of the number of random "
-         "numbers (%8.2e)\n", randomNumberSum,
-         100. * randomNumberSum / float(numberOfRandomNumbers),
-         float(numberOfRandomNumbers));
-  printf("in %5.2lf seconds, measured %zu (%8.2e) divisions used in the "
-         "generation of the %zu (%8.2e) random numbers\n", elapsedTime,
-         numberOfDivisions, float(numberOfDivisions),
-         numberOfRandomNumbers, float(numberOfRandomNumbers));
-
-  return 0;
+        papi.start_counting();
+        for (size_t i = 0; i < numberOfRandomNumbers; ++i) {
+            const double randomNumberBetween0And1 =
+                randomNumberGenerator(randomNumberEngine);
+            randomNumberSum += randomNumberBetween0And1;
+        }
+        const size_t numberOfDivisions = papi.stop_counting()[0];
+  
+        //volatile size_t sumcopy = randomNumberSum;
+  
+        cout << "sum: " << randomNumberSum
+             << ", divisions: "<< numberOfDivisions << endl;
+        assert(numberOfDivisions > 99000000 && numberOfDivisions < 110000000);
+    }
+  
+    return 0;
 }
